2dc2bdfd0c088f8f0e7452cda6cf1f9787f660e9
Kayra3Oz <oeztuerk.kayra@outlook.de>
2024-10-24 18:55:34 +0200

    updated project 1

--- Project 1/usefull_functions.py
+++
@@ -1,6 +0,0 @@
-
-def calculate_average_from_range(range) -> float:
-    #range example: "100-120"
-    lower, upper = map(int, range.split("-"))
-    average = (upper + lower) / 2
-    return average
